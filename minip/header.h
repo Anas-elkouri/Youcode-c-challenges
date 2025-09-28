280fdf5028793791636c7a2c4fbb6a3e4ac1f834
anas <elkoanas86@gmail.com>
2025-09-28 13:33:34 +0200

    remove old folder minip

--- minip/header.h
+++
@@ -1,6 +0,0 @@
-#ifndef HEDEAR_H
-#define HEDEAR_H
-void Ajoute();
-void Modifie();
-void Afficher ();
-#endif
--- minip/vareable.h
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#define MAX 100
-
-char model[MAX][100];
-char statut[MAX][100];
-int id[MAX];
-char date[MAX][100];
-int capacite[100];
-int total;
